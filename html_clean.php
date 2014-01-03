0d1bf285bc34c338a16ac59bc580e9e75049455e
jediliang <jediliang@qq.com>
2014-01-03 18:36:53 +0800

    new function

--- html_clean.php
+++ html_clean.php
@@ -1,5 +1,5 @@
 <?php 
- 	function html_clean($input){
+  	function html_clean($input){
         $tags = array("br","img");
         $pattern = "/<(?'t_whole'\w+)([^<>]*)?\/(\s*)?>|<(?'t_begin'\w+)([^<>]*)?>|<(?'t_error'\w+)([^<>]*)?|<\/(?'t_close'\w+)>/i";
         $matches = array();
@@ -10,37 +10,53 @@
         $t_close = $matches["t_close"];
         $t_whole = $matches["t_whole"];
         
-        //remove the error tag
+
+		$offset = 0;
+		$stack = array();
         $len = count($t_text);
-        for($index = $len-1 ;$index >=0; $index--){
-            if($t_error[$index] != "" && $t_error[$index][0] != ""){
-                $err = $t_text[$index][0];
-                $pos = $t_text[$index][1];
-                $input =substr($input, 0, $pos) .  str_replace($err, "", substr($input,$pos));
-            }
-        }
+		for($i = 0 ; $i < $len; $i++){
+			if($t_error[$i]!="" && $t_error[$i][0] != ""){
+				$text   = $t_text[$i][0];
+				$pos    = $t_text[$i][1] + $offset;
+                $input  = substr($input, 0, $pos) . substr($input, $pos + strlen($text));
+				$offset = $offset - strlen($text);
+				echo "REMOVE:". $text. PHP_EOL;
+			}
+			else if($t_begin[$i]!="" && $t_begin[$i][0] != ""){
+                array_push($stack, $t_begin[$i][0]);
+			}
+			else if($t_close[$i]!="" && $t_close[$i][0] != ""){
+				$text = $t_text[$i][0];
+				$pos  = $t_text[$i][1] + $offset;
+				$tag  = $t_close[$i][0];
+				if(in_array($tag, $stack)){
+					$item = array_pop($stack);
+					while($item != $tag && count($stack) > 0){
+						if(in_array($item, $tags) === false){
+							$temp   = "</{$item}>";
+							$input  = substr($input, 0, $pos) . $temp. substr($input, $pos);
+							$offset = $offset + strlen($temp);
+							echo "INSERT:" . $temp . PHP_EOL;
+						}
+						$item = array_pop($stack);
+						$pos  = $t_text[$i][1] + $offset;
+					}
+				}else{
+					$input  = substr($input, 0, $pos) . substr($input, $pos +strlen($text));
+					$offset = $offset - strlen($text);
+					echo "REMOVE:" . $text . PHP_EOL;
+				}
 				
-        $stack = array();
-        for($index =0; $index < $len; $index++){
-            if($t_begin[$index] != "" && $t_begin[$index][0] != ""){
-                if(in_array($t_begin[$index][0],$tags)){
-                    continue;
-                }
-                array_push($stack, $t_begin[$index][0]);
-            }else if($t_close[$index] != "" && $t_close[$index][0] != ""){
-                if(in_array($t_close[$index][0], $stack)){
-                    $item = array_pop($stack);
-                    while($item != $t_close[$index][0]){
-                        $item = array_pop($stack);
-                    }
-                }
-            }      
-        }
+			}
+		}
+		
         while(count($stack) >0 ){
             $item = array_pop($stack);
-            $input = $input . "</{$item}>";
+			if(in_array($item, $tags) === false){
+				$input = $input . "</{$item}>";
+				echo "APPEND:" . "</{$item}>" . PHP_EOL;
+			}
         }
         return $input;
     }
-
 ?>
